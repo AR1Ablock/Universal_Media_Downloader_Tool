f838ff5f7731c94c5b5b19145ab2b0037fe97b07
AR1Ablock <78879680+AR1Ablock@users.noreply.github.com>
2026-03-03 05:12:18 +0500

    Update DownloadContext.cs

--- Downloader_Backend_(Both_Stacks_Here)/Data/DownloadContext.cs
+++ Downloader_Backend_(Both_Stacks_Here)/Data/DownloadContext.cs
@@ -3,37 +3,18 @@ using Downloader_Backend.Model;
 
 namespace Downloader_Backend.Data
 {
-    public class DownloadContext(DbContextOptions<DownloadContext> options) : DbContext(options)
+public class DownloadContext(DbContextOptions<DownloadContext> options) : DbContext(options)
+{
+    public DbSet<DownloadJob> DownloadJobs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public DbSet<DownloadJob> DownloadJobs { get; set; }
+        base.OnModelCreating(modelBuilder);
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Configure DownloadJob entity
-            modelBuilder.Entity<DownloadJob>()
-                .HasKey(d => d.Id);
-
-            modelBuilder.Entity<DownloadJob>()
-                .Property(d => d.Id)
-                .IsRequired();
-
-            modelBuilder.Entity<DownloadJob>()
-                .Property(d => d.Url)
-                .IsRequired();
-
-            modelBuilder.Entity<DownloadJob>()
-                .Property(d => d.Format)
-                .IsRequired();
-
-            modelBuilder.Entity<DownloadJob>()
-                .Property(d => d.Key)
-                .IsRequired();
-
-            modelBuilder.Entity<DownloadJob>()
-                .Property(d => d.CreatedAt)
-                .HasDefaultValueSql("datetime('now')");
-        }
+        // Only keep what attributes can't express
+        modelBuilder.Entity<DownloadJob>()
+            .Property(d => d.CreatedAt)
+            .HasDefaultValueSql("CURRENT_TIMESTAMP"); // optional, if you want SQL default
     }
+}
 }
